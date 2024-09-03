e0992cd78608e517df162f3a6c399c5c5337ae84
yara.el3dawy <139906491+YaraEladawy1@users.noreply.github.com>
2024-09-03 14:01:40 +0300

    UBD problem

--- problems.cpp
+++ problems.cpp
@@ -55,3 +55,65 @@ int main()
 {
    printFullName(GetFullName(ReadName()));
 }
+
+
+problem 7
+#include <iostream>
+using namespace std;
+
+   int ReadNumber()
+   {
+       int num;
+       cout<<"please enter the number"<<endl;
+       cin>> num;
+       return num;
+   }
+
+   float calculateNumber(int num)
+   {
+       return (float) num / 2;
+   }
+
+   void printNumber(int num)
+   {
+       cout<<"half of" << num<< "is" <<calculateNumber(num)<<endl;
+    }
+
+int main()
+{
+    printNumber(ReadNumber());
+}
+
+problem 8
+#include <iostream>
+using namespace std;
+
+enum enMark {pass=2 , fail=1};
+int readMark ()
+{
+    int mark;
+    cout<<"please enter your mark"<<endl;
+    cin>>mark;
+    return mark;
+}
+
+int calculateMark(int mark)
+{
+   if (mark>50)
+    return enMark::pass;
+   else
+    return enMark::fail;
+}
+
+void printMark(int mark)
+{
+    if(calculateMark(mark)==enMark::pass)
+        cout<<"u passed";
+    else
+        cout<<"u failed";
+}
+
+int main()
+{
+  printMark(readMark());
+}
