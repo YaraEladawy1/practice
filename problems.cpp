d041eddd04aa556eba011385ff0f24e2b5ed44af
yara.el3dawy <139906491+YaraEladawy1@users.noreply.github.com>
2024-09-07 17:34:36 +0200

    Update problems.cpp
    UBD problem.cpp

--- problems.cpp
+++ problems.cpp
@@ -117,3 +117,174 @@ int main()
 {
   printMark(readMark());
 }
+
+
+problem 9
+#include <iostream>
+using namespace std;
+  int ReadNumbers(int& num1 , int& num2 , int& num3)
+  {
+      cout<<"please enter number 1"<<endl;
+      cin>>num1;
+      cout<<"please enter number 2"<<endl;
+      cin>>num2;
+      cout<<"please enter number 3"<<endl;
+      cin>>num3;
+  }
+
+  int CalculateNumber(int num1 , int num2 , int num3)
+  {
+      return num1 + num2 + num3;
+  }
+void printNumbers(int total)
+{
+    cout<<"the sum ="<<total<<endl;
+}
+
+
+int main()
+{
+    int num1 , num2 , num3 ;
+    ReadNumbers(num1 , num2 ,  num3);
+    printNumbers(CalculateNumber( num1 ,  num2 ,  num3));
+}
+
+problem 10
+#include <iostream>
+using namespace std;
+  int ReadNumbers(int& num1 , int& num2 , int& num3)
+  {
+      cout<<"please enter number 1"<<endl;
+      cin>>num1;
+      cout<<"please enter number 2"<<endl;
+      cin>>num2;
+      cout<<"please enter number 3"<<endl;
+      cin>>num3;
+  }
+
+  int CalculateNumber(int num1 , int num2 , int num3)
+  {
+      return num1 + num2 + num3;
+  }
+
+  float sumofmark (int num1 , int num2 , int num3)
+  {
+      return (float)CalculateNumber(num1 , num2 , num3) / 3;
+  }
+void printNumbers(int total)
+{
+    cout<<"the sum ="<<total<<endl;
+}
+
+
+int main()
+{
+    int num1 , num2 , num3 ;
+    ReadNumbers(num1 , num2 ,  num3);
+    printNumbers(sumofmark( num1 ,  num2 ,  num3));
+}
+
+problem 12
+#include <iostream>
+using namespace std;
+
+int ReadNum(int& num1,int& num2)
+{
+    cout<<"please enter first number"<<endl;
+    cin>>num1;
+
+    cout<<"please enter second number"<<endl;
+    cin>>num2;
+}
+
+int CheckMaxnum(int num1,int num2)
+{
+    if(num1>num2)
+    return num1;
+    else
+    return num2;
+}
+
+void PrintNum(int max)
+{
+    cout<<"the max number is"<<" "<<max<<endl;
+}
+
+int main()
+{
+  int num1 , num2;
+  ReadNum(num1,num2);
+  PrintNum(CheckMaxnum(num1,num2));
+}
+
+
+problem14
+
+#include <iostream>
+using namespace std;
+
+int ReadNum(int& num1,int& num2)
+{
+    cout<<"enter number1"<<endl;
+    cin>>num1;
+
+    cout<<"enter num2"<<endl;
+    cin>>num2;
+}
+
+int swap(int& num1,int& num2)
+{ 
+    int temp;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+}
+
+void PrintNum (int num1,int num2)
+{
+    cout<<"number1 is"<<num1<<endl;
+
+    cout<<"number2 is"<<num2<<endl;
+}
+
+
+int main()
+{
+    int num1,num2;
+    ReadNum(num1,num2);
+    PrintNum(num1,num2);
+    swap(num1,num2);
+    PrintNum(num1,num2);
+
+
+}
+
+problem 15
+#include <iostream>
+using namespace std;
+
+int ReadIt(float &hight, float &width)
+{
+    cout << "please enter the hight" << endl;
+    cin >> hight;
+
+    cout << "please enter the width" << endl;
+    cin >> width;
+}
+
+int CalculateIt(float hight, float width)
+{
+    return hight * width;
+}
+
+void PrintIt(int Area)
+{
+    cout << "regtangl area =" << " " << Area << endl;
+}
+
+int main()
+{
+    float hight, width;
+    ReadIt(hight, width);
+    PrintIt(CalculateIt(hight, width));
+}
